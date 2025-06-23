e23673658c4296f4d6f16d95691d4ddb34b0849d
LeonardoTrapani <leonard.trapani@gmail.com>
2025-06-23 19:46:09 +0200

    remove pokemon images + new waybar

--- Configs/.user.zsh
+++ Configs/.user.zsh
@@ -3,11 +3,11 @@
 # Check if the interactive shell option is set
 if [[ $- == *i* ]]; then
     # This is a good place to load graphic/ascii art, display system information, etc.
-    if command -v pokego >/dev/null; then
-        pokego --no-title -r 1,3,6
-    elif command -v pokemon-colorscripts >/dev/null; then
-        pokemon-colorscripts --no-title -r 1,3,6
-    elif command -v fastfetch >/dev/null; then
+    # if command -v pokego >/dev/null; then
+    #     pokego --no-title -r 1,3,6
+    # elif command -v pokemon-colorscripts >/dev/null; then
+    #     pokemon-colorscripts --no-title -r 1,3,6
+    if command -v fastfetch >/dev/null; then
         if do_render "image"; then
             fastfetch --logo-type kitty
         fi
