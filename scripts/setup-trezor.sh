b9997f714648cbed0fe65e44ba01d9ef3496da85
leonardotrapani <ciao@leotrapani.com>
2025-12-20 13:38:12 +0100

    new dotfiles

--- scripts/setup-trezor.sh
+++ scripts/setup-trezor.sh
@@ -1,7 +1,7 @@
 echo "Checking for existing Trezor Suite…"
 APP_DIR="$HOME/.local/bin"
 APP_PATH="$APP_DIR/Trez.AppImage"
-APP_URL="https://data.trezor.io/suite/releases/desktop/latest/Trezor-Suite-25.7.4-linux-x86_64.AppImage"
+APP_URL="https://data.trezor.io/suite/releases/desktop/latest/Trezor-Suite-25.10.2-linux-x86_64.AppImage"
 
 mkdir -p "$APP_DIR"
 
